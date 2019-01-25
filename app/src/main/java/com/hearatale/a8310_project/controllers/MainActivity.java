a4cf363631b3dd1941eff6dc241d24d41f7c998b
Erica Rabinovich <ericar13@live.com>
2019-01-24 19:21:19 -0500

    Main and Login Screen layouts complete

--- app/src/main/java/com/hearatale/a8310_project/controllers/MainActivity.java
+++ app/src/main/java/com/hearatale/a8310_project/controllers/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hearatale.a8310_project.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.hearatale.a8310_project.R;
 import model.User;
@@ -10,11 +14,26 @@ import model.UserList;
 public class MainActivity extends AppCompatActivity {
     private static User user;
     private static UserList userList;
+    private ImageButton play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // go to the login page when play button is pressed
+        play = (ImageButton) findViewById(R.id.Play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+    }
+
+    public void openLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     public static User getUser() {return user;}
