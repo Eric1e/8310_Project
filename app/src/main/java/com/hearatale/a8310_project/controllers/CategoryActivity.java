b049c31909f671d67cfaa53d8e6bdb592baae801
Nathaniel <Cody78n@gmail.com>
2019-04-08 18:38:35 -0400

    added mother visits and broke thumbnails for now

--- app/src/main/java/com/hearatale/a8310_project/controllers/CategoryActivity.java
+++ app/src/main/java/com/hearatale/a8310_project/controllers/CategoryActivity.java
@@ -15,6 +15,8 @@ public class CategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_category);
     }
 
+    /*Changes screen view to Mother Category
+    Passes in file type of m_ to populate rhyme list*/
     public void onMotherClick(View view) {
         Intent intent = new Intent(this, VideoListActivity.class);
         intent.putExtra("category", "m_");
@@ -22,10 +24,21 @@ public class CategoryActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /*Changes screen view to Father Category
+    Passes in file type of f_ to populate rhyme list*/
     public void onFatherClick(View view) {
         Intent intent = new Intent(this, VideoListActivity.class);
         intent.putExtra("category", "f_");
         intent.putExtra("title", "Father Goose");
+        startActivity(intent);
+    }
+
+    /*Changes screen view to Father Category
+    Passes in file type of f_ to populate rhyme list*/
+    public void onMotherVisitClick(View view) {
+        Intent intent = new Intent(this, VideoListActivity.class);
+        intent.putExtra("category", "mv_");
+        intent.putExtra("title", "Mother Goose Visit");
         startActivity(intent);
     }
 }
--- app/src/main/java/com/hearatale/a8310_project/controllers/VideoListActivity.java
+++ app/src/main/java/com/hearatale/a8310_project/controllers/VideoListActivity.java
@@ -1,27 +1,24 @@
 package com.hearatale.a8310_project.controllers;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.hearatale.a8310_project.R;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
-import model.FileManager;
 import model.Media;
 
 public class VideoListActivity extends AppCompatActivity {
 
     private RecyclerView videoList;
 
+    //First time setup
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String catTitle = "";
@@ -30,15 +27,20 @@ public class VideoListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_rhymes);
         Bundle extras = getIntent().getExtras();
+
+        //Pull out the passed in information from previous screen
         if (extras != null) {
             catTitle = extras.getString("title");
             catType = extras.getString("category");
         }
+        //Grab video list ID from view and then complete setup followed by populating the list
         videoList = findViewById(R.id.videoList);
         videoList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        videoList.setAdapter(new VideoListAdapter(createMediaList(catType), catType));
+        String path = this.getFilesDir().getAbsolutePath();
+        videoList.setAdapter(new VideoListAdapter(path, createMediaList(catType), catType));
     }
 
+    //Creates rhyme list using passed in string prefix for type
     public ArrayList<Media> createMediaList(String pre) {
         ArrayList<Media> mediaList = new ArrayList<>();
         Field[] fields = R.raw.class.getFields();
@@ -46,7 +48,7 @@ public class VideoListActivity extends AppCompatActivity {
         try {
             for (Field field : fields) {
                 String name = field.getName();
-                if (name.substring(0,2).equals(pre)) {
+                if (name.substring(0,pre.length()).equals(pre)) {
                     Media media = new Media(Integer.toString(field.getInt(field)));
                     mediaList.add(media);
                 }
