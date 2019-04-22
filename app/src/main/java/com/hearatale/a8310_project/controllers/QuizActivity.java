149f8086da297f0ea9285ad0ae2490864b1e052a
Tom <tthole726@gmail.com>
2019-04-22 01:39:37 -0400

    Implementation of Quiz page

--- app/src/main/java/com/hearatale/a8310_project/controllers/QuizActivity.java
+++ app/src/main/java/com/hearatale/a8310_project/controllers/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.hearatale.a8310_project.controllers;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.hearatale.a8310_project.R;
@@ -15,33 +17,40 @@ import com.hearatale.a8310_project.R;
 import model.Question;
 import model.Quiz;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Random;
 
-public class QuizActivity extends AppCompatActivity {
-//    private Button answer1 = findViewById(R.id.answer1);
-//    private Button answer2 = findViewById(R.id.answer2);
-//    private Button answer3 = findViewById(R.id.answer3);
-//    private Button answer4 = findViewById(R.id.answer4);
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public void fade1(View view) {
-        CardView card1 = findViewById(R.id.card1);
+    private Button answer1;
+    private Button answer2;
+    private Button answer3;
+    private Button answer4;
+    private CardView card1, card2, card3, card4;
+    private ImageButton next_question;
+    private Quiz quiz;
+    private int rhyme_number = 1;
+    private int answer_number;
+    private int question_index;
+
+    public void fade1() {
         card1.animate().alpha(0f).setDuration(2000);
     }
 
-    public void fade2(View view) {
-        CardView card2 = findViewById(R.id.card2);
+    public void fade2() {
         card2.animate().alpha(0f).setDuration(2000);
     }
 
-    public void fade3(View view) {
-        CardView card3 = findViewById(R.id.card3);
+    public void fade3() {
         card3.animate().alpha(0f).setDuration(2000);
     }
 
-    public void fade4(View view) {
-        CardView card4 = findViewById(R.id.card4);
+    public void fade4() {
         card4.animate().alpha(0f).setDuration(2000);
     }
 
@@ -50,17 +59,62 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        Uri uri = Uri.parse("android.resource://com.hearatale.a8310_project/"+R.raw.question_001);
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, uri);
-        mediaPlayer.start();
+        card1 = findViewById(R.id.card1);
+        card2 = findViewById(R.id.card2);
+        card3 = findViewById(R.id.card3);
+        card4 = findViewById(R.id.card4);
 
-        // need to instantiate both the quiz and associated questions here
-//        Quiz quiz = new Quiz();
-//        for (Question q : quiz.getQuestions()) {
-//            ArrayList<String> temp_answers = q.getAnswers();
-//            Collections.shuffle(temp_answers);
-//            updateView(q.getText(), temp_answers, q.getAnswers().get(0));
-//        }
+        answer1 = findViewById(R.id.answer1);
+        answer2 = findViewById(R.id.answer2);
+        answer3 = findViewById(R.id.answer3);
+        answer4 = findViewById(R.id.answer4);
+        next_question = findViewById(R.id.next_question);
+
+        answer1.setOnClickListener(this);
+        answer2.setOnClickListener(this);
+        answer3.setOnClickListener(this);
+        answer4.setOnClickListener(this);
+        next_question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateView();
+            }
+        });
+
+        InputStream is = getResources().openRawResource(R.raw.quizzes);
+        //StringBuilder build = new StringBuilder();
+        ArrayList<String> questions = new ArrayList<>();
+        ArrayList<String> answers = new ArrayList<>();
+        ArrayList<Integer> audio_files = new ArrayList<>();
+        try {
+            BufferedReader buff = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = buff.readLine()) != null) {
+                if (line.length() >= 3 && line.substring(0, 3).equals("[V]") && Integer.parseInt(line.substring(4)) == rhyme_number){
+                    while (!((line = buff.readLine()).substring(0, 1).equals("1"))) {}
+                    questions.add(line.substring(3));
+                    while (!((line = buff.readLine()).substring(0, 1).equals("["))) {
+                        answers.add(line);
+                        if ((line = buff.readLine()).substring(0, 1).equals("[")) {
+                            break;
+                        }
+                        questions.add(line.substring(3));
+                    }
+                    break;
+                }
+            }
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        } finally {
+            try {is.close();} catch (Throwable ignore) {}
+        }
+        audio_files.add(R.raw.question_001);
+        audio_files.add(R.raw.question_002);
+        audio_files.add(R.raw.question_003);
+        audio_files.add(R.raw.question_004);
+        quiz = new Quiz(audio_files, questions, answers);
+        question_index = 0;
+        updateView();
     }
 
     public void onHomeClick(View v) {
@@ -73,12 +127,87 @@ public class QuizActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-//    public void updateView(String text, ArrayList<String> answers, String answer) {
-//        TextView question = findViewById(R.id.question);
-//        question.setText(text);
-//        answer1.setText(answers.get(0));
-//        answer2.setText(answers.get(1));
-//        answer3.setText(answers.get(2));
-//        answer4.setText(answers.get(3));
-//    }
+    public void updateView() {
+        if (question_index >= quiz.getQuestions().size()) {
+            Intent intent = new Intent(this, CategoryActivity.class);
+            startActivity(intent);
+        }
+        next_question.setVisibility(View.INVISIBLE);
+        Question q = quiz.getQuestions().get(question_index);
+        Uri uri = Uri.parse("android.resource://com.hearatale.a8310_project/"+q.getAudio());
+        ArrayList<String> temp_answers = new ArrayList<>(Arrays.asList(q.getAnswers()));
+        Collections.shuffle(temp_answers);
+        answer_number = temp_answers.indexOf(q.getAnswers()[0]);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, uri);
+        TextView question = findViewById(R.id.question);
+        question.setText(q.getText());
+        card1.setAlpha(1f);
+        card2.setAlpha(1f);
+        card3.setAlpha(1f);
+        card4.setAlpha(1f);
+        answer1.setText(temp_answers.get(0));
+        answer2.setText(temp_answers.get(1));
+        answer3.setText(temp_answers.get(2));
+        answer4.setText(temp_answers.get(3));
+        mediaPlayer.start();
+        question_index++;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.answer1:
+                if (answer_number == 0) {
+                    answer1.setText("Correct!");
+                    fade2();
+                    fade3();
+                    fade4();
+                    next_question.setVisibility(View.VISIBLE);
+                } else {
+                    answer1.setText("That's wrong, \ntry again!");
+                }
+                break;
+
+            case R.id.answer2:
+                if (answer_number == 1) {
+                    answer2.setText("Correct!");
+                    fade1();
+                    fade3();
+                    fade4();
+                    next_question.setVisibility(View.VISIBLE);
+                } else {
+                    answer2.setText("That's wrong, \ntry again!");
+                }
+                break;
+
+            case R.id.answer3:
+                if (answer_number == 2) {
+                    answer3.setText("Correct!");
+                    fade1();
+                    fade2();
+                    fade4();
+                    next_question.setVisibility(View.VISIBLE);
+                } else {
+                    answer3.setText("That's wrong, \ntry again!");
+                }
+                break;
+
+            case R.id.answer4:
+                if (answer_number == 3) {
+                    answer4.setText("Correct!");
+                    fade1();
+                    fade2();
+                    fade3();
+                    next_question.setVisibility(View.VISIBLE);
+                } else {
+                    answer4.setText("That's wrong, \ntry again!");
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }
 }
--- app/src/main/java/model/Quiz.java
+++ app/src/main/java/model/Quiz.java
@@ -3,11 +3,13 @@ package model;
 import java.util.ArrayList;
 
 public class Quiz {
-    private ArrayList<Question> questions;
+    private ArrayList<Question> questions = new ArrayList<>();
 
-    public Quiz(String audio_file, String question_text) {
-        Question question = new Question(audio_file, question_text);
-        this.questions.add(question);
+    public Quiz(ArrayList<Integer> audio_files, ArrayList<String> question_text, ArrayList<String> answers_text) {
+        for (int i = 0; i < question_text.size(); i++) {
+            Question question = new Question(audio_files.get(i), question_text.get(i), answers_text.get(i));
+            this.questions.add(question);
+        }
     }
 
     public ArrayList<Question> getQuestions() {
